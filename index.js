87179a6062d9918fec1bce11a5bdaae384498a3a
huynhsamha <huynhsamha@gmail.com>
2018-03-06 16:52:36 +0700

    fix error

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 function toSnakeCase(str) {
 
-	if (!str && str != '') return null;
+	if (!str) return '';
 
 	return String(str)
-		.replace(/^[^A-Za-z]*|[^A-Za-z0-9]*$/g, '')
+		.replace(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g, '')
 		.replace(/([a-z])([A-Z])/g, (m, a, b) => a + '_' + b.toLowerCase())
 		.replace(/[^A-Za-z0-9]+|_+/g, '_')
 		.toLowerCase();
